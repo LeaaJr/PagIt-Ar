20b532d1393675a72d3820db62188d4e679fc1f3
LeaaJr <leeitooocabj@gmail.com>
2024-09-23 15:35:41 +0200

    partonerouter

--- src/App.js
+++ src/App.js
@@ -4,24 +4,29 @@ import MainSection from "./Components/MainSection";
 import ProductSection from "./Components/ProductSection";
 import Footer from "./Components/Footer";
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { /* faFacebook, faTwitter, faGoogle */ faInstagram, } from '@fortawesome/free-brands-svg-icons';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Routes, Route } from "react-router-dom";
+import Productos from './pages/Productos';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <main>
-        <MainSection />
-        <ProductSection />
-      </main>
+      <Routes>
+        {/* Ruta para la página principal (puedes ajustarla según lo que necesites) */}
+        <Route path="/" element={
+          <main>
+            <MainSection />
+            <ProductSection />
+          </main>
+        } />
+        
+        {/* Ruta para la página de productos */}
+        <Route path="/productos" element={<Productos />} />
+      </Routes>   
       <Footer />
     </div>
   );
 }
-
-
-library.add(faInstagram,)
 
 export default App;
--- src/Components/ProductSection.js
+++ src/Components/ProductSection.js
@@ -1,37 +1,49 @@
 import React from "react";
+import {Link } from "react-router-dom";
 
 function ProductSection() {
   return (
     <div className="main-custom-class">
       <div className="custom-inner-class">
-        <div className="my-3 py-3">
-          <h2 className="display-5">Producto1</h2>
-          <p className="lead">subtitulo de producto</p>
+          <div className="my-3 py-3">
+            <h2 className="display-5">Producto1</h2>
+            <p className="lead">subtitulo de producto</p>
+          </div>
+          <Link to="/productos">
+              <div 
+              className="bg-body-tertiary shadow-sm mx-auto"
+              style={{ width: "80%", height: "300px", borderRadius: "21px 21px 0 0" }}
+          ></div> 
+          </Link>
+
         </div>
-        <div
-          className="bg-body-tertiary shadow-sm mx-auto"
-          style={{ width: "80%", height: "300px", borderRadius: "21px 21px 0 0" }}
-        ></div>
-      </div>
-      <div className="custom-inner-classtwo">
-        <div className="my-3 p-3">
-          <h2 className="display-5">Producto2</h2>
-          <p className="lead">subtitulo de producto</p>
+        <div className="custom-inner-classtwo">
+          <div className="my-3 p-3">
+            <h2 className="display-5">Producto2</h2>
+            <p className="lead">subtitulo de producto</p>
+          </div>
+
+          <Link to="/productos">
+              <div 
+              className="bg-body-tertiary shadow-sm mx-auto"
+              style={{ width: "80%", height: "300px", borderRadius: "21px 21px 0 0" }}
+          ></div> 
+          </Link>
+
         </div>
-        <div
-          className="bg-dark shadow-sm mx-auto"
-          style={{ width: "80%", height: "300px", borderRadius: "21px 21px 0 0" }}
-        ></div>
-      </div>
-      <div className="custom-inner-classthree">
-        <div className="my-3 p-3">
-          <h2 className="display-5">Producto2</h2>
-          <p className="lead">subtitulo de producto</p>
-        </div>
-        <div
-          className="bg-dark shadow-sm mx-auto"
-          style={{ width: "80%", height: "300px", borderRadius: "21px 21px 0 0" }}
-        ></div>
+          <div className="custom-inner-classthree">
+            <div className="my-3 p-3">
+              <h2 className="display-5">Producto2</h2>
+              <p className="lead">subtitulo de producto</p>
+          </div>
+
+          <Link to="/productos">
+              <div 
+              className="bg-body-tertiary shadow-sm mx-auto"
+              style={{ width: "80%", height: "300px", borderRadius: "21px 21px 0 0" }}
+          ></div> 
+          </Link>
+
       </div>
     </div>
   );
