ccc7725f913c60123a2c931ee8ad16d8841fd024
LeaaJr <leeitooocabj@gmail.com>
2024-09-24 14:18:56 +0200

    cardtwo

--- src/pages/Productos.js
+++ src/pages/Productos.js
@@ -3,45 +3,133 @@ import '../Cards.css';
 
 const CardDeck = () => {
   return (
-    <div className="card-deck">
-      <div className="card">
-        <img className="card-img-top" src="..." alt="Card image cap" />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-          </p>
-          <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </p>
+    <>
+      <div className="custom-container-card">
+        <div className="second-custom-class-card">
+          <h1 className="custom-display-card">Pasiones Argentinas en Italia</h1>
+          <h3 className="custom-subtitle-card">Productos auténticos que traen el alma de Argentina a Italia</h3>
         </div>
       </div>
-      <div className="card">
-        <img className="card-img-top" src="..." alt="Card image cap" />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            This card has supporting text below as a natural lead-in to additional content.
-          </p>
-          <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </p>
+
+      <div className="card-deck">
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This card has supporting text below as a natural lead-in to additional content.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
         </div>
       </div>
-      <div className="card">
-        <img className="card-img-top" src="..." alt="Card image cap" />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
-          </p>
-          <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </p>
+
+      <div className="card-deck">
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This card has supporting text below as a natural lead-in to additional content.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="card-deck">
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This card has supporting text below as a natural lead-in to additional content.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
+            </p>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default CardDeck;
+
